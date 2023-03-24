30423a2cb5abb519bdc3415e2b3900aa6bbcec4d
Preston Moore <moorepjay@gmail.com>
2023-03-24 02:08:22 -0400

    Remove redundant pages.

--- pages/archive.py
+++ pages/archive.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc
 
-dash.register_page(__name__)
+# dash.register_page(__name__)
 
 layout = html.Div(children=[
     html.H1(children='This is our Archive page'),
