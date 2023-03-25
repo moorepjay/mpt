671d20daa79d30219ee77825762dd6cc4fa4937c
Preston Moore <moorepjay@gmail.com>
2023-03-25 11:47:08 -0400

    Update user for mongodb (PROD).

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 def get_database(database):
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     # TODO: Use python-dotenv!!!!
-    CONNECTION_STRING = "mongodb+srv://moorepj:RL05NFeWyexis43T|@agsrmt.6jcivn0.mongodb.net/test"
+    CONNECTION_STRING = "mongodb+srv://moorepj:RL05NFeWyexis43T@agsrmt.6jcivn0.mongodb.net/test"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
