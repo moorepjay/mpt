4c9b2be250c68155fe469991672aca1b1901f150
Preston Moore <moorepjay@gmail.com>
2023-03-25 13:17:53 -0400

    Update user for mongodb (PROD).

--- pages/analytics.py
+++ pages/analytics.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc, callback, Input, Output
 
-# dash.register_page(__name__)
+dash.register_page(__name__)
 
 layout = html.Div(children=[
     html.H1(children='This is our Analytics page'),
